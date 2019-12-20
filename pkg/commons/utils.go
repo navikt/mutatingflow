4a7842badced234c4f9835c6eeecac25ceb19bfd
Vattekar <42180319+erikvatt@users.noreply.github.com>
2019-12-20 12:52:45 +0100

    skrivefeil

--- pkg/commons/utils.go
+++ pkg/commons/utils.go
@@ -1,7 +1,6 @@
 package commons
 
 import (
-	log "github.com/sirupsen/logrus"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -60,9 +59,6 @@ func MutationRequired(metadata metav1.ObjectMeta, annotation string) bool {
 	if !ok {
 		return false
 	}
-
-	log.Infof("Pod: Annotations %s", annotations):q
-
 
 	status := annotations[StatusKey]
 	if strings.ToLower(status) == "injected" {
